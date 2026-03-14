dcc163b22560ac20d472b1165dd70a48cc577976
arvgl715 <arvgl715@muxen2-106.ad.liu.se>
2026-03-14 13:03:11 +0100

    Filter and delta-sigma modulator.

--- src/project.vhdl
+++ src/project.vhdl
@@ -16,10 +16,65 @@ entity tt_um_example is
 end tt_um_example;
 
 architecture Behavioral of tt_um_example is
+
+    constant offset : signed := "01100000000000000000";
+
+    signal input : signed(15 downto 0) := (others => '0');
+    signal offset_input : signed(19 downto 0) := (others => '0');
+    signal filter_output : signed(19 downto 0) := (others => '0');
+    signal output : signed(19 downto 0) := (others => '0');
+    signal y : std_logic;
+    signal filter_input_tmp : unsigned(17 downto 0) := (others => '0');
+    signal filter_input : signed(18 downto 0) := (others => '0');
+
+    signal delay0 : signed(19 downto 0) := (others => '0');
+    signal delay1 : signed(18 downto 0) := (others => '0');
+    signal delay2 : signed(18 downto 0) := (others => '0');
+    signal delay3 : signed(18 downto 0) := (others => '0');
+    signal delay4 : signed(18 downto 0) := (others => '0');
+    signal delay5 : signed(18 downto 0) := (others => '0');
+    signal delay6 : signed(18 downto 0) := (others => '0');
+    signal delay7 : signed(18 downto 0) := (others => '0');
+    signal delay8 : signed(18 downto 0) := (others => '0');
+    signal delay9 : signed(18 downto 0) := (others => '0');
+    signal delay10 : signed(18 downto 0) := (others => '0');
+    signal delay11 : signed(18 downto 0) := (others => '0');
+    signal delay12 : signed(18 downto 0) := (others => '0');
+
+    
 begin
 
+    process(clk)
+    begin
+    if rising_edge(clk) then
+        filter_output <= delay0;
+        delay0 <= resize(filter_input, 20) + resize(shift_right(filter_input, 2), 20) + resize(delay1, 20);
+        delay1 <= delay2 + shift_right(filter_input, 5);
+        delay2 <= delay3;
+        delay3 <= delay4;
+        delay4 <= delay5;
+        delay5 <= delay6 + (shift_right(filter_input, 8) - shift_right(filter_input, 3));
+        delay6 <= delay7 - (shift_right(filter_input, 3));
+        delay7 <= delay8;
+        delay8 <= delay9;
+        delay9 <= delay10;
+        delay10 <= delay11;
+        delay11 <= delay12;
+        delay12 <= shift_right(filter_input, 3) - (shift_right(filter_input, 5) + shift_right(filter_input, 8));
+    end if;
+    end process;
+
+    input <= signed(ui_in & uio_in);
+    offset_input <= signed(offset) + resize(input, 20); 
+    output <= offset_input + filter_output;
+    assert (output(19) = '0');
+    y <= output(18);
+    filter_input_tmp <= unsigned(output(17 downto 0));
+    filter_input <= signed(resize(filter_input_tmp, 19));
+
+
     uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
-    uio_out <= "00000000";
+    uio_out <= "00000000"; 
     uio_oe <= "00000000";
 
 end Behavioral;
